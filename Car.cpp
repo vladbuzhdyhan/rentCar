d66b9aa307bcd66bfedcc5d9de3b52b3d8038717
vladbuzhdyhan <vladyslavbuzhdyhan@gmail.com>
2024-02-20 21:27:46 +0200

    Add static counter, copy and move constructors, overloaded operators to the "Car" class
    If applied, this commit will add static counter (_amountOfCars), copy and move constructors, overloaded operators (+, -, +=, -=, =, << (instead of printInfo), >>) to the "Car" class

--- Car.cpp
+++ Car.cpp
@@ -1,19 +1,65 @@
 #include <iostream>
 #include "Car.h"
 
+int Car::_amountOfCars = 0;
+
 Car::Car(std::string model)
         : Car{model, 100} {}
 
 Car::Car(std::string model, int priceForADay)
-        : _model{model}, _priceForADay{priceForADay} {}
+        : _model{model}, _priceForADay{priceForADay} { _amountOfCars++; }
 
-std::string &Car::getModel() { return _model; }
+Car::Car(const Car &other)
+        : _model{other._model}, _priceForADay(other._priceForADay) { _amountOfCars++; }
 
-int Car::getPriceForADay() { return _priceForADay; }
+Car::Car(Car &&other) noexcept
+        : _model(other._model), _priceForADay(other._priceForADay) { _amountOfCars++; }
+
+const std::string &Car::getModel() const { return _model; }
+
+int Car::getPriceForADay() const { return _priceForADay; }
 
 void Car::setPriceForADay(int priceForADay) { _priceForADay = priceForADay; }
 
-void Car::printInfo() {
-    std::cout << "Model: " << _model << std::endl;
-    std::cout << "Price for a day: " << _priceForADay << std::endl;
+int Car::getAmountOfCars() { return _amountOfCars; }
+
+Car Car::operator+(int num) {
+    _priceForADay += num;
+    return *this;
+}
+
+Car Car::operator-(int num) {
+    _priceForADay -= num;
+    return *this;
+}
+
+Car Car::operator+=(int num) {
+    _priceForADay += num;
+    return *this;
+}
+
+Car Car::operator-=(int num) {
+    _priceForADay -= num;
+    return *this;
+}
+
+Car &Car::operator=(const Car &other) {
+    _priceForADay = other._priceForADay;
+    _model = other._model;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &os, const Car &car) {
+    os << "Model: " << car._model << std::endl;
+    os << "Price for a day: " << car._priceForADay << std::endl;
+    return os;
+}
+
+std::istream &operator>>(std::istream &is, Car &car) {
+    is >> car._priceForADay;
+    return is;
+}
+
+Car::~Car() {
+    _amountOfCars--;
 }
--- Car.h
+++ Car.h
@@ -6,18 +6,39 @@
 class Car {
     std::string _model;
     int _priceForADay;
+    static int _amountOfCars;
 public:
     Car(std::string model);
 
     Car(std::string model, int priceForADay);
 
-    std::string &getModel();
+    Car(const Car &other);
 
-    int getPriceForADay();
+    Car(Car &&other) noexcept;
+
+    const std::string &getModel() const;
+
+    int getPriceForADay() const;
 
     void setPriceForADay(int priceForADay);
 
-    void printInfo();
+    static int getAmountOfCars();
+
+    Car operator+(int num);
+
+    Car operator-(int num);
+
+    Car operator+=(int num);
+
+    Car operator-=(int num);
+
+    Car &operator=(const Car &other);
+
+    friend std::ostream &operator<<(std::ostream &os, const Car &car);
+
+    friend std::istream &operator>>(std::istream &is, Car &car);
+
+    ~Car();
 };
 
 #endif //RENTCAR_CAR_H
